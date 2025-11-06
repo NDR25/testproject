27d7984bcbbda398ba9444f87d27658f6787daef
NDR25 <niels.van.drunen@merford.com>
2025-11-06 14:58:46 +0100

    Update conf.py
    changed directory for logo png file

--- docs/conf.py
+++ docs/conf.py
@@ -106,7 +106,7 @@ html_theme = 'pydata_sphinx_theme'
 html_static_path = ['_static']
 html_css_files = ['css/custom.css']
 html_js_files = ['js/custom.js']
-html_logo = 'C:\Users\NDR\Downloads\soundglass_2.png'
+html_logo = 'docs/soundglass_2.png'
 html_title = "testproject"
 html_favicon = '_static/favicon.ico'
 
